afcc5a4acb192dab1a45d9277cc5b20b7adb580e
carry-machine <sea1014kk@naver.com>
2024-07-01 12:31:42 +0900

    Add files via upload

--- js/login.js
+++ js/login.js
@@ -9,9 +9,51 @@
                     $(".login form .box .user").val('');
                     $(".login form .box .password").val('');
                 }
+            });      
 
-                
-            })        
-        
+
+           
+
+            $(".login .user, .login .password").click(function(){
+                $(".login .box").css({
+                    borderRight: '1px solid #000',
+                    borderTop: '1px solid #000',
+                    borderLeft: '1px solid #000',
+                });
+
+                $(".login .box").last().css({
+                    borderBottom: '1px solid #000',
+                    
+                })
+
+                $(".login .bi-person").css({
+                    color:'#000'
+                })
+                $(".login .bi-key").css({
+                    color:'#000'
+                })
+            });
+
+            
+            
+            // $(".login form").click(function(){
+            //     $(".login .box").css({
+            //         borderRight: '1px solid #ccc',
+            //         borderTop: '1px solid #ccc',
+            //         borderLeft: '1px solid #ccc',
+            //     });
+
+            //     $(".login .box").last().css({
+            //         borderBottom: '1px solid #ccc',
+                    
+            //     })
+
+            //     $(".login .bi-person").css({
+            //         color:'#ccc'
+            //     })
+            //     $(".login .bi-key").css({
+            //         color:'#ccc'
+            //     })
+            // });
         })
         
